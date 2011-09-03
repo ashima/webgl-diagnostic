7c91d5d59fc565241c0198277d9f94fdda20c971
David Sheets <sheets@ashimaarts.com>
2011-09-03 16:27:06 -0700

    remove language code dependency in php conng

--- example/index.php
+++ example/index.php
@@ -2,12 +2,7 @@
 // From http://www.w3.org/QA/2006/02/content_negotiation
 
 include('choose_lang.php');
-if ($chosenlang == "zh") {
-  include 'example.zh.html';
-} else if ($chosenlang == "cy") {
-  include 'example.cy.html';
-} else {
-  include 'example.en.html';
-}
+include('example.'.$chosenlang.'.html');
+
 ?>
    
