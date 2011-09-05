e7d1671f5d44553ab25fb7da362eeda627f8e674
David Sheets <sheets@ashimaarts.com>
2011-09-05 14:18:43 -0700

    php conneg example with proxy-friendly http headers

--- example/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// From http://www.w3.org/QA/2006/02/content_negotiation
-
-include('choose_lang.php');
-header("Content-Language: ".$chosenlang);
-include('example.'.$chosenlang.'.html');
-
-?>
-   
--- example/php/choose_lang.php
+++ example/php/choose_lang.php
@@ -10,13 +10,15 @@ unset($lang_neg_reason); // at the end of the negotiation, this variable will st
                          //  was used to find the best language variant.
 
 // <EDIT(dsheets) 2011-9-3>
+$pagepathprefix = "../example.";
 function lang_of_filename($filename) {
-  return substr($filename,strlen("example."),2);
+  return substr($filename,strlen($pagepathprefix),2);
 }
 
 $defaultlang = "en";
 $translations = array_fill_keys(array_map("lang_of_filename",
-					  glob("example.??.html")),true);
+					  glob("$pagepathprefix??.html")),
+				true);
 // </EDIT>
 
 // first, we check whether the client has sent us, in a cookie,
