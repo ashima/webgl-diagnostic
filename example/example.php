285165c314e6a5c121a075c5e5de29719a6f0488
David Sheets <sheets@ashimaarts.com>
2011-09-05 14:28:21 -0700

    fix php paths

--- example/example.php
+++ example/example.php
@@ -1,11 +1,11 @@
 <?php
 // From http://www.w3.org/QA/2006/02/content_negotiation
 
-include('choose_lang.php');
+include('php/choose_lang.php');
 header("Content-Language: $chosenlang");
 header("Vary: Accept-Language");
 header("Content-Location: example.$chosenlang.html");
-include('example.'.$chosenlang.'.html');
+include('example.$chosenlang.html');
 
 ?>
    
